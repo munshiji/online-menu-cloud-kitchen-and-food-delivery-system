c2acfe725894c40cbad1071c733f8999f563a4cc
zakirIndia <48102616+zakirIndia@users.noreply.github.com>
2021-05-16 10:13:02 +0530

    Create main.css

---
+++ files/main.css
@@ -0,0 +1,8 @@
+Most Advance online restaurant menu, 
+online restaurant menu, 
+free online restaurant menu, 
+free online restaurant menu app, 
+online restaurant menu service provider
+
+
+## Home Page : <a href="https://github.com/munshiji/online-menu-and-cloud-kitchen" target="_blank">menu.munshijji.tech</a>
